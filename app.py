5d5b88f23aebf0bd5106d25ab5609f9a408d9fdf
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-05-21 23:28:46 +0100

    ---

---
+++ app.py
@@ -0,0 +1,14 @@
+from flaskr import app
+from flask import render_template
+
+
+
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
